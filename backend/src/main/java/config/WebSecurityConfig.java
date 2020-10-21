11ff471890692bb9f66d032bcbfa36480e643c36
Marcin Niełacny <you@example.com>
2020-10-21 12:23:51 +0200

    Added package auth

--- backend/src/main/java/config/WebSecurityConfig.java
+++ backend/src/main/java/config/WebSecurityConfig.java
@@ -1,15 +1,33 @@
 package config;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import auth.LogoutSuccess;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
 	protected final Log LOGGER = LogFactory.getLog(getClass());
 	
+	private final CustomUserDetailsService jwtUserDetailsService;
+	private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+	private final LogoutSuccess logoutSuccess;
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+	private final AuthenticationFailureHandler authenticationFailureHandler;
+	  
+	@Value("${jwt.cookie}")
+	private String TOKEN_COOKIE;
+	
 	public void changePassword(String oldPassword, String newPassword) throws Exception {
 		 Authentication currentUser = SecurityContextHolder.getContext().getAuthentication();
-		    String username = currentUser.getName();
+		 String username = currentUser.getName();
 
 		    if (authenticationManagerBean() != null) {
 		      LOGGER.debug("Re-authenticating user '" + username + "' for password change request.");
